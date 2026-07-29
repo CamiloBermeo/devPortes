cbff6b58b27089de5b23ec57cf54f0dd543d64be
Camilo <bermeocamilo12@gmail.com>
2026-07-29 10:29:38 -0500

    fix: estructura completa del proyecto con breve explicacion

---
+++ js/api/canchas.js
@@ -0,0 +1,3 @@
+// en una clase como esta es donde hacermos las peticiones al backend 
+// dependiendo lo que necesitemos especifico de la cancha por ejmplo con metodos
+// como obtenerCanchas(), crearReserva()

5d3e4e9a2d669a7b81d24e2fa83c6c4c56171fb7
Deybid Santiago Moreno Ruiz <ingdeybid.moreno@gmail.com>
2026-08-12 02:01:20 -0500

    validacion de campos en form contact check

--- js/views/contactenos.js
+++ js/views/contactenos.js
@@ -16,13 +16,49 @@ customElements.whenDefined('app-footer').then(() => {
 });
 
 
-//implementacion de formspree
+//implementacion de formspree y validaciones de campos
 const form = document.getElementById("form");
 form.addEventListener("submit", handleSubmit)
+const nombre = document.getElementById("nombre");
+const categoria = document.getElementById("categoria");
+const telefono = document.getElementById("telefono");
+const correo = document.getElementById("correo");
+const asunto = document.getElementById("asunto");
+const mensaje = document.getElementById("mensaje");
 
+function mostrarError(input, mensaje){
+    input.classList.add("is-invalid");
+    input.classList.remove("is-valid");
+
+    let feedback = input.parentNode.querySelector(".invalid-feedback");
+    if(!feedback){
+        feedback = document.createElement("div");
+        feedback.classList.add("invalid-feedback");
+        input.parentNode.appendChild(feedback);
+    }
+    feedback.textContent = mensaje;
+}
 
 async function handleSubmit(event) {
     event.preventDefault();
+    if(nombre.value.trim()==""){
+        mostrarError(nombre, "¡Upsss, este campo no puede estar vacio 😬!")        
+    }
+    if(categoria.value.trim()==""){
+        mostrarError(categoria, "Seguro querrás categorizarla 🙄")        
+    }
+    if(telefono.value.trim()==""){
+        mostrarError(telefono, "¡Nos encantaría llamarte 😢!")        
+    }
+    if(correo.value.trim()==""){
+        mostrarError(correo, "¡Nos encantaría escribirte 😢!")        
+    }
+    if(asunto.value.trim()==""){
+        mostrarError(asunto, "¿Sobre qué es tu consulta? 🤔")        
+    }
+    if(mensaje.value.trim()==""){
+        mostrarError(mensaje, "¡Anímate a escribirnos 😁!")        
+    }
     const formulario = event.target;
     const datos = new FormData(formulario);
     const response = await fetch(formulario.action, {
@@ -41,3 +77,4 @@ async function handleSubmit(event) {
         });
     }
 }
+

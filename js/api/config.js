d279547f4a1206b4a7289b609ee763e19c11dddc
Mauricio Alvarez <297007275+MarAlvGEN@users.noreply.github.com>
2026-09-18 00:01:09 -0500

    Conect to dev back

--- js/api/config.js
+++ js/api/config.js
@@ -2,7 +2,7 @@
 // export const API_URL = 'https://devportes-backend-yvbj.onrender.com/api/v1';
 
 // Develop
-// export const API_URL = 'https://devportes-backend-yvbj.onrender.com/api/v1';
+export const API_URL = 'https://devportes-backend-yvbj.onrender.com/api/v1';
 
 // Local
-export const API_URL = 'http://localhost:8080/api/v1';
+// export const API_URL = 'http://localhost:8080/api/v1';

5284611e0d3fa54ea587dacd837621028dbcce32
Mauricio Alvarez <297007275+MarAlvGEN@users.noreply.github.com>
2026-09-20 14:06:53 -0500

    Connect

--- js/api/config.js
+++ js/api/config.js
@@ -1,5 +1,5 @@
 // Develop
-// export const API_URL = 'https://devportes-backend-yvbj.onrender.com/api/v1';
+export const API_URL = 'https://devportes-backend-yvbj.onrender.com/api/v1';
 
 // Local
-export const API_URL = 'http://localhost:8080/api/v1';
+// export const API_URL = 'http://localhost:8080/api/v1';
